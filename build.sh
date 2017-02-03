c37a196d4f60ca2e889ac9422b534f1167be62f8
Han Manjong <han@manjong.org>
2017-02-03 16:47:37 +0900

    Fix the deployment scripts

--- build.sh
+++ build.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-BOWER_PATH=/usr/local/bin/bower
+BOWER_PATH=/usr/bin/bower
 GIT_PATH=/usr/bin/git
 ROOT_DIR=/var/www/pastel_chat
 DOCKER_PATH=/usr/bin/docker
 DOCKER_COMPOSE_PATH=/usr/local/bin/docker-compose
 
 function build_forge_min_js {
-    local forge_min_js="$ROOT_DIR/app/pastel_chat/static/libs/forge/js/forge.min.js"
+    local forge_min_js="$ROOT_DIR/pastel_chat/static/libs/forge/js/forge.min.js"
     if [ -f "$forge_min_js" ]
     then
         echo "forge_min_js is existed."
     else
-        cd "$ROOT_DIR/app/pastel_chat/static/libs/forge"
+        cd "$ROOT_DIR/pastel_chat/static/libs/forge"
         npm install
         npm run minify
     fi
@@ -26,6 +26,6 @@ $GIT_PATH pull origin master
 $BOWER_PATH install --allow-root
 build_forge_min_js
 
-DOCKER_PATH build -t pastel/pastel-chat .
+$DOCKER_PATH build -t pastel/pastel-chat .
 
-DOCKER_COMPOSE_PATH up -d
+$DOCKER_COMPOSE_PATH up -d
--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-BOWER_PATH=/usr/local/bin/bower
+BOWER_PATH=/usr/bin/bower
 GIT_PATH=/usr/bin/git
 ROOT_DIR=/var/www/pastel_chat
 DOCKER_PATH=/usr/bin/docker
 DOCKER_COMPOSE_PATH=/usr/local/bin/docker-compose
 
-cd $ROOT_DIR/app
+cd $ROOT_DIR
 $GIT_PATH pull origin master
 $BOWER_PATH install --allow-root
 
 /bin/chown -R www-data:www-data $ROOT_DIR
-DOCKER_COMPOSE_PATH up -d
+$DOCKER_COMPOSE_PATH up -d
+
+curl -X POST --data-urlencode \
+    'payload={"channel": "#dev", "username": "배포하는애", "text": "린더봇을 성공적으로 배포했습니다. 하핳 확인해보셈: http://linder.kr", "icon_emoji": ":fire:"}' \
+    https://hooks.slack.com/services/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
