9c4a21eabe51aeda3b3978c23748efc814b96729
Han Manjong <han@manjong.org>
2017-03-08 04:06:56 +0900

    Apply the docker remote API

--- Dockerfile
+++ Dockerfile
@@ -68,3 +68,4 @@ COPY ./ ${PASTEL_CHAT_APP_PATH}/
 RUN apt-get autoremove -y && apt-get clean
 
 ENTRYPOINT /var/www/pastel_chat/scripts/start.sh
+EXPOSE 80
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
-DOCKER_PATH=/usr/bin/docker
-DOCKER_COMPOSE_PATH=/usr/local/bin/docker-compose
+DOCKER_APP_NAME=pastel-chat
+TARGET_DEPLOY_TCP=tcp://172.31.27.217:2375
+EXIST_BLUE=$(DOCKER_HOST=${TARGET_DEPLOY_TCP} docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
 
-function build_forge_min_js {
-    local forge_min_js="$ROOT_DIR/pastel_chat/static/libs/forge/js/forge.min.js"
-    if [ -f "$forge_min_js" ]
-    then
-        echo "forge_min_js is existed."
-    else
-        cd "$ROOT_DIR/pastel_chat/static/libs/forge"
-        npm install
-        npm run minify
-    fi
-}
+docker build -t registry.hiddentrack.co/${DOCKER_APP_NAME} .
+docker push registry.hiddentrack.co/${DOCKER_APP_NAME}
 
-bower install --allow-root
-build_forge_min_js
+if [ -z "$EXIST_BLUE" ]; then
+    DOCKER_HOST=${TARGET_DEPLOY_TCP} docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml pull
+    DOCKER_HOST=${TARGET_DEPLOY_TCP} docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d
 
-$DOCKER_COMPOSE_PATH up -d
+    sleep 10
 
-curl -X POST --data-urlencode \
-    'payload={"channel": "#dev", "username": "배포하는애", "text": "린더봇을 성공적으로 배포했습니다. 하핳 확인해보셈: http://linder.kr", "icon_emoji": ":fire:"}' \
-    https://hooks.slack.com/services/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
+    DOCKER_HOST=${TARGET_DEPLOY_TCP} docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
+else
+    DOCKER_HOST=${TARGET_DEPLOY_TCP} docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml pull
+    DOCKER_HOST=${TARGET_DEPLOY_TCP} docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d
+
+    sleep 10
+
+    DOCKER_HOST=${TARGET_DEPLOY_TCP} docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
+fi
+
+#curl -X POST --data-urlencode \
+#    'payload={"channel": "#dev", "username": "배포하는애", "text": "린더봇을 성공적으로 배포했습니다. 하핳 확인해보셈: http://linder.kr", "icon_emoji": ":fire:"}' \
+#    https://hooks.slack.com/services/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
