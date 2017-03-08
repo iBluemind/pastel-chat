bf4fee4f1d797367479bf2d447d7fa9c7aef7bb2
Han Manjong <han@manjong.org>
2017-03-08 17:43:39 +0900

    Fix the Blue-Green Deployment

--- scripts/docker/build.sh
+++ scripts/docker/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 ROOT_PATH=`pwd`
+DOCKER_APP_NAME=pastel-chat
 
-docker build -t pastel/pastel-chat $ROOT_PATH
+docker build -t registry.hiddentrack.co/${DOCKER_APP_NAME} ${ROOT_PATH}
+docker push registry.hiddentrack.co/${DOCKER_APP_NAME}
